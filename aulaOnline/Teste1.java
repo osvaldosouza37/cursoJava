c234f0cce59bb9b123a7a79e3e67b1b2a3aa3ac4
osvaldo souza <osvaldo.react37@gmail.com>
2021-03-20 08:49:30 -0300

    primeiro commit

---
+++ aulaOnline/Teste1.java
@@ -0,0 +1,21 @@
+package aulaOnline;
+
+import java.util.Scanner;
+
+public class Teste1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome.");
+		
+		System.out.println(scan.nextLine());
+		
+		System.out.println("Digite sua idade");
+		
+		System.out.println(scan.nextInt());
+			
+	}
+
+}
